d52f65c04bfcb1c3ef4f214d0716db7c45d97c64
codeeLifer <1341687052@qq.com>
2021-07-03 19:29:03 +0800

    upload orm

---
+++ Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"po"},{"l":"pool"},{"l":"sorm_bean"},{"l":"sorm_core"},{"l":"sorm_utils"},{"l":"test"},{"l":"VO"},{"l":"所有程序包","url":"allpackages-index.html"}]
---
+++ readme.md
@@ -0,0 +1,1 @@
+# 2020年所写的一个java与MYSQL数据库连接的ORM小脚本，low版本jdbcTemplated
